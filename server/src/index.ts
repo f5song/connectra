b96a17e7a2ff73c52c0433e50919a2daaa719920
f5song <folkkykytak@gmail.com>
2024-11-01 03:37:55 +0700

    connect db

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,22 @@
-// run backend --> npx ts-node src/index.ts
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import pool from './db'; 
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+app.get("/", async (req: Request, res: Response) => {
+    try {
+        const result = await pool.query('SELECT * FROM users'); // ทดสอบการดึงข้อมูลจากตาราง users
+        res.send(result.rows);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+    console.log(`[server]: Server is running at http://localhost:${port}`);
 });
